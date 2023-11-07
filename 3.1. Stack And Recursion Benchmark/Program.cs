f5c61e3271ad863e5031c4734ac22eb7152ebc36
Denys Boryhin <denys.boryhin@nixsolutions.com>
2023-11-07 22:46:02 +0100

    benchmark has been added

---
+++ 3.1. Stack And Recursion Benchmark/Program.cs
@@ -0,0 +1,4 @@
+﻿using _3._1._Stack_And_Recursion_Benchmark;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<StackRecurision>();
---
+++ 3.1. Stack And Recursion Benchmark/StackRecurision.cs
@@ -0,0 +1,67 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace _3._1._Stack_And_Recursion_Benchmark;
+
+[MemoryDiagnoser]
+[RankColumn]
+public class StackRecurision
+{
+    [Params(5, 40)]
+    public int Value;
+
+    [Benchmark]
+    public void FibonacciStack()
+    {
+        CalculateFibonacciStack(Value);
+    }
+
+    [Benchmark]
+    public void FibonacciRecursion()
+    {
+        CalculateFibonacciRecursion(Value);
+    }
+
+    private static ulong CalculateFibonacciRecursion(int n)
+    {
+        if (n <= 0)
+        {
+            throw new ArgumentException("n must be a positive long.");
+        }
+        else if (n == 1 || n == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return CalculateFibonacciRecursion(n - 1) + CalculateFibonacciRecursion(n - 2);
+        }
+    }
+
+    private static ulong CalculateFibonacciStack(int n)
+    {
+        if (n <= 0)
+        {
+            throw new ArgumentException("n must be a positive long.");
+        }
+        else if (n == 1 || n == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            Stack<ulong> fibStack = new Stack<ulong>();
+            fibStack.Push(1);
+            fibStack.Push(1);
+
+            for (int i = 3; i <= n; i++)
+            {
+                ulong prev = fibStack.Pop();
+                ulong prevPrev = fibStack.Peek();
+                fibStack.Push(prev);
+                fibStack.Push(prev + prevPrev);
+            }
+
+            return fibStack.Peek();
+        }
+    }
+}
